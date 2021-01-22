075a71e16b46c9cf72eb678a038794116b4cd5a8
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-01-22 18:29:19 -0400

    css encapsulation

--- src/components/Itunessearch.vue
+++ src/components/Itunessearch.vue
@@ -4,7 +4,7 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
     />
-    <div class="example" style="margin:auto;max-width:300px">
+    <div class="example" style="margin:auto;max-width:50%">
       <input type="text" placeholder="Search.." name="search2" />
       <button type="submit"><i class="fa fa-search"></i></button>
     </div>
@@ -17,41 +17,4 @@ export default {
 };
 </script>
 
-<style scoped>
-body {
-  font-family: Arial;
-}
-
-* {
-  box-sizing: border-box;
-}
-.example input[type=text] {
-  padding: 10px;
-  font-size: 17px;
-  border: 1px solid grey;
-  float: left;
-  width: 80%;
-  background: #f1f1f1;
-}
-
-.example button {
-  float: left;
-  width: 20%;
-  padding: 10px;
-  background: #2196F3;
-  color: white;
-  font-size: 17px;
-  border: 1px solid grey;
-  border-left: none;
-  cursor: pointer;
-}
-.example button:hover {
-  background: #0b7dda;
-}
-
-.example::after {
-  content: "";
-  clear: both;
-  display: table;
-}
-</style>
+<style src="@/assets/search.css" scoped></style>
