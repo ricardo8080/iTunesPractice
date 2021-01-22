6f0f869add32412ca96312d0e8a50179787e42df
Ricardo <ricardo.fernandez.bo.2000@ieee.org>
2021-01-22 15:50:44 -0400

    eslint + prettier working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <img alt="Logo" src="../assets/logo.png" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    //HelloWorld
   }
 };
 </script>
+
+<style scoped>
+.home {
+  vertical-align: middle;
+
+  }
+</style>
